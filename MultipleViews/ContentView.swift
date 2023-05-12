a947710bf1b35e6b77f5d1c8474aa4d8c2a203bd
Vincenzo Silva Fadda <vincenzofadda@gmail.com>
2023-05-12 16:40:27 -0300

    initial commit

--- MultipleViews/ContentView.swift
+++ MultipleViews/ContentView.swift
@@ -9,18 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("Bem vindo(a) ao aplicativo de controle de chamada")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                NavigationLink(destination: SecondView()) {
+                    Text("Entrar como professor")
+                        .font(.title)
+                        //.padding()
+                        .background(.clear)
+                        .foregroundColor(.blue)
+                        //.cornerRadius(10)
+                }
+                
+                NavigationLink(destination: SecondView()) {
+                    Text("Entrar como aluno")
+                        .font(.title)
+                        //.padding()
+                        .background(.clear)
+                        .foregroundColor(.blue)
+                        //.cornerRadius(10)
+                }
+                .padding()
+            }
+            .navigationBarTitle("Controle de chamada")
+            .padding()
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
