42b8c545cc57ea2bbf78d3f549e6c48016d6b2c9
Vincenzo Silva Fadda <vincenzofadda@gmail.com>
2023-05-12 14:29:24 -0300

    Initial Commit

---
+++ MultipleViews/MultipleViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleViewsApp.swift
+//  MultipleViews
+//
+//  Created by user on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MultipleViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
